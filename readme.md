76c356d38f47822925e5857cf47915764a605395
attaramajesta <78683973+attaramajesta@users.noreply.github.com>
2024-02-12 23:48:27 +0700

    feat main.py readme.md folder

---
+++ readme.md
@@ -0,0 +1,31 @@
+# Tugas Kecil 1 Strategi Algoritma Teknik Informatika
+# Semester II Tahun 2023/2024
+
+## Deskripsi :
+A program designed to find the optimal solution for Cyberpunk 2077's Breach Protocol mini-game, where players hack local ICE systems. It utilizes the Brute Force algorithm to navigate through matrices, sequences, and buffer sizes, aiming to maximize rewards. The program provides prompt outputs of the maximum reward, buffer contents, token coordinates, and execution time, offering players efficient hacking strategies
+
+## How To Use :
+1. Ensure you have Python installed along with an appropriate IDE. Developer used ver 3.8.10 64-bit.
+2. Make sure your environment is compatible with NumPy as the code utilizes it.
+3. Simply run the source code namely 'main.py' in folder 'src' to use the program.
+
+## Developer :
+Attara Majesta Ayub       / 13522139
+
+## Stucture :
+src
+|--back-end
+    |--_pyache_
+    |--CBIR
+    |--image
+    |--instance
+    |--static
+    |--templates
+        |--venv
+        |app.py
+|--front-end    
+    |--css
+    |--font
+    |--images
+    |--js
+    |index.html
---
+++ src/main.py
@@ -0,0 +1,378 @@
+# DRAFT
+# import time
+# import sys
+# import random
+
+# def generate_input():
+#     """
+#     Generate input via CLI.
+#     """
+#     while True:
+#         try:
+#             # Read command-line arguments
+#             num_unique_tokens = int(input("\nJumlah token: "))
+#             tokens = input("Masukkan token (dipisahkan spasi): ").split()
+#             if len(tokens) < num_unique_tokens:
+#                 raise ValueError("Not compatible with the number of unique tokens.")
+#             buffer_size = int(input("Masukkan ukuran buffer: "))
+#             matrix_size = tuple(map(int, input("Masukkan ukuran matriks (baris kolom): ").split()))
+#             num_sequences = int(input("Masukkan jumlah sekuens: "))
+#             max_sequence_size = int(input("Masukkan ukuran maksimal sekuens: "))
+
+#             matrix = []
+#             for _ in range(matrix_size[0]):
+#                 row = [random.choice(tokens) for _ in range(matrix_size[1])]
+#                 matrix.append(row)
+#             print("\nMATRIKS:\n")
+#             for row in matrix:
+#                 print(' '.join(row))
+
+#             # Generate sequences
+#             sequences = []
+#             print("SEQUENCES:\n", ' '.join(sequence))
+#             for _ in range(num_sequences):
+#                 sequence = [tokens[i % num_unique_tokens] for i in range(max_sequence_size)]
+#                 reward = random.choice([10, 20, 30])
+#                 sequences.append((''.join(sequence), reward))
+
+#             return buffer_size, matrix, sequences
+
+#         except ValueError:
+#             print("Error: Input incompatible. Restarting input process... \n\n")
+
+# def read_input(filename):
+#     with open(filename, 'r') as file:
+#         buffer_size = int(file.readline())
+#         matrix_size = tuple(map(int, file.readline().split()))
+#         matrix = [file.readline().split() for _ in range(matrix_size[0])]
+#         num_of_sequences = int(file.readline())
+#         sequences = []
+#         for _ in range(num_of_sequences):
+#             sequence = ''.join(file.readline().split())
+#             reward = int(file.readline())
+#             sequences.append((sequence, reward))
+#     return buffer_size, matrix, sequences
+
+# def KMPSearch(pat, txt):
+#     M = len(pat)
+#     N = len(txt)
+#     lps = [0]*M
+#     j = 0
+#     found = 0
+    
+#     computeLPSArray(pat, M, lps)
+
+#     i = 0
+#     while (N - i) >= (M - j):
+#         if pat[j] == txt[i]:
+#             i += 1
+#             j += 1
+
+#         if j == M:
+#             found += 1
+#             j = lps[j-1]
+
+#         elif i < N and pat[j] != txt[i]:
+#             if j != 0:
+#                 j = lps[j-1]
+#             else:
+#                 i += 1     
+
+#     return found           
+
+# def computeLPSArray(pat, M, lps):
+#     len = 0
+#     lps[0] = 0
+#     i = 1
+
+#     while i < M:
+#         if pat[i] == pat[len]:
+#             len += 1
+#             lps[i] = len
+#             i += 1
+#         else:
+#             if len != 0:
+#                 len = lps[len-1]
+#             else:
+#                 lps[i] = 0
+#                 i += 1
+
+# def calculate_reward(path, sequences):
+#     reward = 0
+#     path_str = ''.join(path)
+#     for sequence, reward_value in sequences:
+#         found = KMPSearch(sequence, path_str)
+#         if found != 0:
+#             reward += found * reward_value
+#     return reward
+
+# def find_paths(matrix, sequences, x, y, visited, path, max_path, max_reward):
+#     rows = len(matrix)
+#     cols = len(matrix[0])
+
+#     if x < 0 or x >= rows or y < 0 or y >= cols or visited[x][y]:
+#         return
+
+#     visited[x][y] = True
+#     path.append((x, y, matrix[x][y])) 
+
+#     if len(path) == buffer_size:
+#         reward = calculate_reward([id for _, _, id in path], sequences)
+#         if reward > max_reward[0]:
+#             max_reward[0] = reward
+#             max_path.clear()
+#             max_path.extend(path)
+#     else:
+#         if len(path) % 2 == 0:
+#             for j in range(-(rows-1),rows-1):
+#                 if j != 0:
+#                     find_paths(matrix, sequences, x, y + j, visited, path, max_path, max_reward)
+#         elif len(path) % 2 == 1:
+#             for i in range(-(cols-1),cols-1):
+#                 if i != 0:
+#                     find_paths(matrix, sequences, x + i, y, visited, path, max_path, max_reward)
+
+#     # Backtrack
+#     visited[x][y] = False
+#     path.pop()
+
+# def find_all_paths(buffer_size, matrix, sequences):
+#     rows = len(matrix)
+#     cols = len(matrix[0])
+#     max_reward = [0]
+#     max_path = []
+
+#     for j in range(cols):
+#         visited = [[False for _ in range(cols)] for _ in range(rows)]
+#         find_paths(matrix, sequences, 0, j, visited, [], max_path, max_reward)
+
+#     return max_path
+
+# def save_all_paths(filename, max_path, execution_time, reward):
+#     with open(filename, 'w') as file:
+#         file.write(f'Reward: {reward}\n')
+#         file.write('Path: ' + ' -> '.join([f'{y+1},{x+1}' for x, y, _ in max_path]) + '\n')
+#         file.write('IDs: ' + ' '.join([id for _, _, id in max_path]) + '\n\n')
+#         file.write(execution_time + '\n')
+
+# if __name__ == "__main__":
+#     print("\nWELCOME!\n\nApakah Anda ingin menggunakan input dari file atau generate input?")
+#     print("1. File\n2. Generate\n")
+#     choice = input("Pilihan: ")
+#     if choice == "1":
+#         buffer_size, matrix, sequences = read_input('input.txt')
+#     elif choice == "2":
+#         buffer_size, matrix, sequences = generate_input() 
+#     else:
+#         print("Error: Pilihan tidak valid. Keluar... \n\n")
+#         sys.exit(1)
+
+#     start_time = time.time()
+#     max_path = find_all_paths(buffer_size, matrix, sequences)
+#     reward = calculate_reward([id for _, _, id in max_path], sequences)
+#     execution_time = f'{(time.time() - start_time) * 1000:.2f} ms'
+
+#     if reward == 0:
+#         print("\nTidak ada path yang memenuhi syarat.")
+#     else:
+#         print(f'\n{reward}')
+#         print(' '.join([id for _, _, id in max_path]))
+#         print('\n'.join([f'{y+1},{x+1}' for x, y, _ in max_path]) + '\n')
+    
+#     print(execution_time + '\n')
+#     save = input("Apakah Anda ingin menyimpan hasil ke file? (y/n): ")
+#     if save == "y":
+#         save_all_paths('output.txt', max_path, execution_time, reward)
+#         print("Berhasil disimpan ke 'output.txt'.\n\n")
+#     else:
+#         print("Keluar... \n\n")
+#         sys.exit(1)
+
+import time
+import sys
+import random
+import numpy as np
+
+def generate_input():
+    while True:
+        try:
+            num_unique_tokens = int(input("\nJumlah token: "))
+            tokens = input("Masukkan token (dipisahkan spasi): ").split()
+            if len(tokens) < num_unique_tokens:
+                raise ValueError("Tidak sesuai dengan jumlah token unik.")
+            buffer_size = int(input("Masukkan ukuran buffer: "))
+            matrix_size = tuple(map(int, input("Masukkan ukuran matriks (baris kolom): ").split()))
+            num_sequences = int(input("Masukkan jumlah sekuens: "))
+            max_sequence_size = int(input("Masukkan ukuran maksimal sekuens: "))
+
+            matrix = np.random.choice(tokens, matrix_size)
+
+            sequences = []
+            for _ in range(num_sequences):
+                sequence = ''.join(random.choices(tokens, k=max_sequence_size))
+                reward = random.choice([10, 20, 30])
+                sequences.append((sequence, reward))
+
+            return buffer_size, matrix, sequences
+
+        except ValueError:
+            print("Error: Input tidak sesuai. Proses input diulang... \n\n")
+
+def read_input(filename):
+    with open(filename, 'r') as file:
+        buffer_size = int(file.readline())
+        matrix_size = tuple(map(int, file.readline().split()))
+        matrix = np.array([file.readline().split() for _ in range(matrix_size[0])])  # Menggunakan np.array
+        num_of_sequences = int(file.readline())
+        sequences = []
+        for _ in range(num_of_sequences):
+            sequence = ''.join(file.readline().split())
+            reward = int(file.readline())
+            sequences.append((sequence, reward))
+    return buffer_size, matrix, sequences
+
+def KMPSearch(pat, txt, lps_cache):
+    if (pat, txt) in lps_cache:
+        return lps_cache[(pat, txt)]
+
+    M = len(pat)
+    N = len(txt)
+    lps = [0] * M
+    j = 0
+    found = 0
+
+    length = 0
+    lps[0] = 0
+    i = 1
+
+    while i < M:
+        if pat[i] == pat[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+
+    i = 0
+    while (N - i) >= (M - j):
+        if pat[j] == txt[i]:
+            i += 1
+            j += 1
+
+        if j == M:
+            found += 1
+            j = lps[j - 1]
+
+        elif i < N and pat[j] != txt[i]:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+
+    lps_cache[(pat, txt)] = found
+    return found
+
+
+def calculate_reward(path, sequences, lps_cache):
+    reward = 0
+    path_str = ''.join(path)
+    for sequence, reward_value in sequences:
+        found = KMPSearch(sequence, path_str, lps_cache)
+        if found != 0:
+            reward += found * reward_value
+    return reward
+
+def find_paths(matrix, sequences, x, y, visited, path, max_path, max_reward, lps_cache):
+    rows, cols = matrix.shape
+
+    if x < 0 or x >= rows or y < 0 or y >= cols or visited[x][y]:
+        return
+
+    visited[x][y] = True
+    path.append((x, y, matrix[x, y]))
+
+    if len(path) == buffer_size:
+        reward = calculate_reward([id for _, _, id in path], sequences, lps_cache)
+        if reward > max_reward[0]:
+            max_reward[0] = reward
+            max_path.clear()
+            max_path.extend(path)
+    else:
+        if len(path) % 2 == 0:
+            for j in range(-(rows - 1), rows - 1):
+                if j != 0:
+                    find_paths(matrix, sequences, x, y + j, visited, path, max_path, max_reward, lps_cache)
+        elif len(path) % 2 == 1:
+            for i in range(-(cols - 1), cols - 1):
+                if i != 0:
+                    find_paths(matrix, sequences, x + i, y, visited, path, max_path, max_reward, lps_cache)
+
+    visited[x][y] = False
+    path.pop()
+
+def find_all_paths(buffer_size, matrix, sequences, lps_cache):
+    rows, cols = matrix.shape
+    max_reward = [0]
+    max_path = []
+    visited = np.zeros_like(matrix, dtype=bool)
+
+    for j in range(cols):
+        find_paths(matrix, sequences, 0, j, visited, [], max_path, max_reward, lps_cache)
+
+    return max_path
+
+def save_all_paths(filename, max_path, execution_time, reward):
+    with open(filename, 'w') as file:
+        file.write(f'Reward: {reward}\n')
+        file.write('Path: ' + ' -> '.join([f'{y + 1},{x + 1}' for x, y, _ in max_path]) + '\n')
+        file.write('IDs: ' + ' '.join([id for _, _, id in max_path]) + '\n\n')
+        file.write(execution_time + '\n')
+
+if __name__ == "__main__":
+    print("\nWELCOME!\n\nApakah Anda ingin menggunakan input dari file atau generate input?")
+    print("1. File\n2. Generate\n")
+    choice = input("Pilihan: ")
+    if choice == "1":
+        buffer_size, matrix, sequences = read_input('input.txt')
+    elif choice == "2":
+        buffer_size, matrix, sequences = generate_input()
+    else:
+        print("Error: Pilihan tidak valid. Keluar... \n\n")
+        sys.exit(1)
+
+    lps_cache = {}
+    start_time = time.time()
+    max_path = find_all_paths(buffer_size, matrix, sequences, lps_cache)
+    reward = calculate_reward([id for _, _, id in max_path], sequences, lps_cache)
+    execution_time = f'{(time.time() - start_time) * 1000:.2f} ms'
+
+    if choice == "2":
+        print("\nMATRIX:\n")
+        for i in range(matrix.shape[0]):
+            for j in range(matrix.shape[1]):
+                print(matrix[i, j], end=' ')
+            print()
+        print("\nSEQUENCES:\n")
+        for seq, reward in sequences:
+            print(seq)
+            print(reward)
+
+    if reward == 0:
+        print("\nTidak ada path yang memenuhi syarat.")
+    else:
+        print(f'\n{reward}')
+        print(' '.join([id for _, _, id in max_path]))
+        print('\n'.join([f'{y + 1},{x + 1}' for x, y, _ in max_path]) + '\n')
+
+    print(execution_time + '\n')
+    save = input("Apakah Anda ingin menyimpan hasil ke file? (y/n): ")
+    if save == "y":
+        save_all_paths('output.txt', max_path, execution_time, reward)
+        print("Berhasil disimpan ke 'output.txt'.\n\n")
+    else:
+        print("Keluar... \n\n")
+        sys.exit(1)
